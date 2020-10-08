6b43ffed427442871b87ff59bbbc0c16ebb0c855
Neilgeorge03 <49652125+Neilgeorge03@users.noreply.github.com>
2020-10-08 16:32:23 -0400

    Create QuadraticEquation.java

---
+++ QuadraticEquation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public    class    QuadraticEquation{
+    public static Scanner sc;
+    public static void main(String[]args) {
+        sc= new Scanner(System.in);
+        System.out.print("the equation to be is :");
+
+        double    a=    sc.nextDouble();
+        double    b=    sc.nextDouble();
+        double    c=    sc.nextDouble();
+
+        double answer1;
+        answer1= Math.sqrt((bb-4ac));
+        answer1=-b+answer1;
+        answer1= answer1/(2a);
+        System.out.println("X1="+answer1);
+
+        double    answer2;
+        answer2= Math.sqrt((bb-4ac));
+        answer2=-b-answer2;
+        answer2= answer2/(2a);
+        System.out.println("X2="+answer2);
+
+
+}
+}
+
+Message #general
